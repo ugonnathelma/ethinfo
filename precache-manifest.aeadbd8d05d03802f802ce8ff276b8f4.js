420e7b943cead841137da910180726b22b3a6909
Ugonna Ofoegbu <ugonnaofoegbu@MacBook-Pro-2.fritz.box>
2020-07-12 11:34:49 +0200

    Updates

---
+++ precache-manifest.aeadbd8d05d03802f802ce8ff276b8f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13818ba8872ae1269b1b8354943c351c",
+    "url": "/ethinfo/index.html"
+  },
+  {
+    "revision": "a6db5fca81528c18cc43",
+    "url": "/ethinfo/static/css/main.6cc523a1.chunk.css"
+  },
+  {
+    "revision": "b776f670bcad60811025",
+    "url": "/ethinfo/static/js/2.d3b1ab3e.chunk.js"
+  },
+  {
+    "revision": "92176c57ddb7d60e49aec5073e4c0be9",
+    "url": "/ethinfo/static/js/2.d3b1ab3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6db5fca81528c18cc43",
+    "url": "/ethinfo/static/js/main.1bf2aecf.chunk.js"
+  },
+  {
+    "revision": "dc413fa042c8f68bd559",
+    "url": "/ethinfo/static/js/runtime-main.ec318841.js"
+  }
+]);
